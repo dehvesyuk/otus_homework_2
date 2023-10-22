2cde0a3bb518ae982103d46d527baf9dddcf78da
dmitry-khvesyuk <d.khvesyuk@incrdbl.me>
2023-10-22 16:55:19 +0300

    clients_interests request handler done

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 import datetime
+import functools
 import hashlib
 import json
-import functools
 import logging
 import uuid
-from typing import List
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from optparse import OptionParser
+from typing import Dict
 
 from fields import (
     ClientIDsField, DateField, CharField, EmailField, PhoneField, BirthDayField,
-    GenderField, ArgumentsField
+    GenderField, ArgumentsField, Field, ValidationError
 )
 from scoring import get_interests
 
@@ -42,49 +42,53 @@ GENDERS = {
 
 class Request:
     def __init__(self, **params):
+        self._api_fields = []
         for key, value in params.items():
-            setattr(self, key, value)
-        self.validate()
+            field_type = type(getattr(self, key))
+            setattr(self, key, field_type(value))
+            self._api_fields.append(getattr(self, key))
+
+        for field in self._api_fields:
+            field.validate(field.value)
 
 
-
-class ClientsInterestsRequest(Request):
-    client_ids = ClientIDsField(required=True)
-    date = DateField(required=False, nullable=True)
-
-    def validate(self):
-        # TODO add try: except block with raise
-        return self._client_ids != []
-
-    @property
-    def context(self):
-        return {
-            "nclients": len(self._client_ids)
-        }
-
-    def generate_response(self):
-        return get_interests({}, self._client_ids)
-
-
-class OnlineScoreRequest:
-    first_name = CharField(required=False, nullable=True)
-    last_name = CharField(required=False, nullable=True)
-    email = EmailField(required=False, nullable=True)
-    phone = PhoneField(required=False, nullable=True)
-    birthday = BirthDayField(required=False, nullable=True)
-    gender = GenderField(required=False, nullable=True)
-
-
-class MethodRequest:
+class MethodRequest(Request):
     account = CharField(required=False, nullable=True)
     login = CharField(required=True, nullable=True)
+    method = CharField(required=True, nullable=True)
     token = CharField(required=True, nullable=True)
     arguments = ArgumentsField(required=True, nullable=True)
-    method = CharField(required=True, nullable=False)
 
     @property
     def is_admin(self):
         return self.login == ADMIN_LOGIN
+
+
+class ClientsInterestsRequest(Request):
+    client_ids = ClientIDsField(required=True, nullable=False)
+    date = DateField(required=False, nullable=True)
+
+    def validate(self):
+        if not self.client_ids.value:
+            raise ValidationError("Client_ids field cannot be empty")
+
+    @property
+    def context(self) -> Dict:
+        return {
+            "nclients": len(self.client_ids.value)
+        }
+
+    def generate_response(self):
+        return get_interests({}, self.client_ids.value)
+
+
+class OnlineScoreRequest(Request):
+    phone = PhoneField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
 
 
 def check_auth(request):
@@ -116,20 +120,22 @@ def method_handler(request, ctx, store):
     return response, code
 
 
-def clients_interests_handler(request, ctx, store):
-    logging.info(f"request: {request}, ctx: {ctx}, store: {store}")
-    #TODO add auth decorator
-    handler = ClientsInterestsRequest(request["body"])
-    context = handler.context
+def clients_interests_handler(request: Dict, ctx: Dict, store):
+    logging.info(f"request: {type(request)}, ctx: {ctx}, store: {store}")
+    prep_request: MethodRequest = request.get("body")
+
+    handler = ClientsInterestsRequest(**prep_request.arguments.value)
+    ctx.update(handler.context)
+    handler.validate()
     response = handler.generate_response()
-    try:
-        handler.validate()
-        return response, 200
-    except Exception as e:
-        return {"error": f"ошибка валидации, {e}"}, 422
+    return response, 200
 
 
-def online_score_handler(request, ctx, store):
+def online_score_handler(request: MethodRequest, ctx, store):
+    method_request = OnlineScoreRequest(
+        **request["body"]
+    )
+    logging.info(f"request: {type(request)}, ctx: {ctx}, store: {store}")
     #TODO add handler and auth decorator
     response, code = None, None
     return response, code
@@ -162,8 +168,14 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
             if path in self.router:
                 try:
                     response, code = self.router[path](
-                        {"body": request, "headers": self.headers},
+                        {
+                            "body": MethodRequest(**request),
+                            "headers": self.headers
+                        },
                         context, self.store)
+                except ValidationError as e:
+                    logging.exception("Validation error: %s" % e)
+                    code = BAD_REQUEST
                 except Exception as e:
                     logging.exception("Unexpected error: %s" % e)
                     code = INTERNAL_ERROR
--- fields.py
+++ fields.py
@@ -2,18 +2,21 @@ from datetime import datetime
 from typing import Dict, List
 
 
+class ValidationError(ValueError):
+    message = ""
+
+
 class Field:
-    def __init__(self, required=False, nullable=True):
+    def __init__(self, value=None, required=False, nullable=True):
         self.required = required
         self.nullable = nullable
-        self.value = None
-        self.date = None
+        self.value = value
 
     def base_validate(self, value):
         if not self.nullable and value is None:
-            raise ValueError("Expected not None")
+            raise ValidationError("Expected not None")
         if self.required and not value:
-            raise ValueError("Field is required")
+            raise ValidationError("Field is required")
 
     def validate(self, value):
         ...
@@ -23,14 +26,14 @@ class CharField(Field):
 
     def validate(self, value):
         if not (type(value) == str):
-            raise ValueError('Char Field got non-string type')
+            raise ValidationError('Char Field got non-string type')
 
 
 class DigitField(Field):
 
     def validate(self, value):
         if not (type(value) == int):
-            raise ValueError('Digit field got non-digit type')
+            raise ValidationError('Digit field got non-digit type')
 
 
 class DateField(Field):
@@ -38,22 +41,22 @@ class DateField(Field):
     def validate(self, value):
         try:
             self.date = datetime.strptime(value, "%d.%m.%Y")
-        except ValueError:
-            raise ValueError("Date field don't match format: DD.MM.YYYY")
+        except ValidationError:
+            raise ValidationError("Date field don't match format: DD.MM.YYYY")
 
 
 class EmailField(Field):
 
     def validate(self, value):
         if "@" not in value:
-            raise ValueError('Email field got not valid email')
+            raise ValidationError('Email field got not valid email')
 
 
 class PhoneField(Field):
 
     def validate(self, value):
         if len(str(value)) != 11 or str(value)[0] != 7:
-            raise ValueError('Phone field not valid phone number')
+            raise ValidationError('Phone field not valid phone number')
 
 
 class BirthDayField(DateField):
@@ -61,10 +64,10 @@ class BirthDayField(DateField):
     def validate(self, value):
         super().validate(value)
         if (datetime.now() - self.date).days/365 >= 70:
-            raise ValueError('More than 70 years have passed since birth date')
+            raise ValidationError('More than 70 years have passed since birth date')
 
         elif (datetime.now() - self.date).days/365 < 0:
-            raise ValueError('You have not born yet')
+            raise ValidationError('You have not born yet')
 
 
 class GenderField(DigitField):
@@ -72,18 +75,18 @@ class GenderField(DigitField):
     def validate(self, value):
         super().validate(value)
         if self.value not in [0, 1, 2]:
-            raise ValueError("Gender field don't match next values: 0, 1, 2")
+            raise ValidationError("Gender field don't match next values: 0, 1, 2")
 
 
 class ClientIDsField(Field):
 
     def validate(self, value):
         if not isinstance(value, List):
-            raise ValueError("ClientIDs field is not valid List type")
+            raise ValidationError("ClientIDs field is not valid List type")
 
         for val in value:
             if not isinstance(val, int):
-                raise ValueError(
+                raise ValidationError(
                     "At least one element of ClientIDs array is not valid "
                     "integer type")
 
@@ -92,4 +95,4 @@ class ArgumentsField(Field):
 
     def validate(self, value):
         if not isinstance(value, Dict):
-            raise TypeError('Arguments field is not valid dict (json)')
+            raise ValidationError('Arguments field is not valid dict (json)')
